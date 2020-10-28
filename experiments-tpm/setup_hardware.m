7a536e45b37e8e677a0a79df98d4735a6a721874
Daniel <danielcox16@hotmail.com>
2020-10-28 11:23:22 +0100

    Set wavelength to 690nm

--- experiments-tpm/setup_hardware.m
+++ experiments-tpm/setup_hardware.m
@@ -26,7 +26,7 @@ if active_devices.slm
     clear sopt;
     
     % SLM options
-    lambda = 700;                           % laser wavelength (in nm)
+    lambda = 690;                           % laser wavelength (in nm)
     sopt.slm_patch = 1;                     % patch number used for wavefront correction
     sopt.N_diameter = 18;                   % number of segments along the diagonal of the SLM pattern
     sopt.displayPort = 1;
