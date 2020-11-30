8d65983ac5874253c9f0a45b37af06db228fb4a3
Daniel <danielcox16@hotmail.com>
2020-11-30 21:50:29 +0100

    Divide into chunks according to file size

--- process-tpm/split_matfile_SLM_segments.m
+++ process-tpm/split_matfile_SLM_segments.m
@@ -1,16 +1,14 @@
 % Split file by SLM segments
-function split_matfile_SLM_segments(matfilepath, savedir)
+function split_matfile_SLM_segments(matfilepath, savedir, numchunks)
     % Split file by SLM segments
     %
     % Some measurement data is packed into one big file. This function splits it up and saves
     % into mat files of manageable size.
     %
     % Input:
-    % matfile       Char array or string. Path to mat file to be processed.
-    % outfolder     Char array or string. Path to folder where output mat files will be saved.
-    % consoletext   Char array or string. Text to display on console during process. Optional.
-    %
-    % Requires: eta function from the utilities repo.
+    % matfilepath   Char array or string. Path to mat file to be processed.
+    % savedir       Char array or string. Path to folder where output mat files will be saved.
+    % chunksize     Positive Integer. The big arrays will be loaded in chunks of this size.
     
     [~, infilename, ~] = fileparts(matfilepath);
     warning('off', 'MATLAB:MKDIR:DirectoryExists');
@@ -19,31 +17,51 @@ function split_matfile_SLM_segments(matfilepath, savedir)
     numchars = 0;
     load(matfilepath, 'copt_ft', 'copt_img', 'sopt', 'p');          % Load metadata
     inputfile = matfile(matfilepath);                               % Create matfile object
-    [~,~,S,G] = size(inputfile, 'frames_img');                      % Number of segments
+    [Nx,Ny,S,G] = size(inputfile, 'frames_img');                    % Number of segments
     
     if G < 1
         % Note: For some corrupt files, an empty dimension might be reported
         warning('Empty array in %s\nCorrupt file?', matfilepath);
         return
     end
+
     
-    % Loop over segments
-    for s = 1:S
-        % Construct output path
-        outfilename = sprintf('%s_%03i.mat', infilename, s);
-        savepath = fullfile(savedir, outfilename);
+    chunksize = ceil(S/numchunks);                                  % Size of an array chunk
+    s = 1;                                                          % Segment number
+    
+    % Loop over chunks
+    for c = 1:numchunks
+        % Load data chunk
         
-        % Print progress
-        fprintf(repmat('\b', [1 numchars]))
-        numchars = fprintf('Output file %i/%i\n%s\n', s, S, outfilename);
+        chunkend = min(s+chunksize-1, S);                               % Index of chunk end
+        thischunksize = length(s:chunkend);                             % Size of this chunk
+        bytestr = bytes2str(Nx*Ny*G*thischunksize*4, '%.0f');           % Formatted char array
+        numchars = numchars + fprintf('Loading chunk (%s) %i/%i...\n', bytestr, c, numchunks);
         
-        % Extract segment data
-        frames_slmslice_ft  = inputfile.frames_ft(:,:,s,1:G);
-        frames_slmslice_img = inputfile.frames_img(:,:,s,1:G);
+        frames_chunk_ft  = inputfile.frames_ft(:, :, s:chunkend, 1:G);  % Load chunk ft frames
+        frames_chunk_img = inputfile.frames_img(:,:, s:chunkend, 1:G);  % Load chunk img frames
         
-        % Save the extracted data and metadata
-        save(savepath, '-v7.3', 's', 'frames_slmslice_ft', 'frames_slmslice_img',...
-            'copt_ft', 'copt_img', 'sopt', 'p');
+        
+        for sc = 1:thischunksize
+    
+            % Construct output path
+            outfilename = sprintf('%s_%03i.mat', infilename, s);
+            savepath = fullfile(savedir, outfilename);
+
+            % Print progress
+            fprintf(repmat('\b', [1 numchars]))
+            numchars = fprintf('Output file %i/%i\n%s\n', s, S, outfilename);
+
+            % Extract segment data
+            frames_slmslice_ft  = frames_chunk_ft(:,:,sc,:);
+            frames_slmslice_img = frames_chunk_img(:,:,sc,:);
+
+            % Save the extracted data and metadata
+            save(savepath, '-v7.3', 's', 'frames_slmslice_ft', 'frames_slmslice_img',...
+                'copt_ft', 'copt_img', 'sopt', 'p');
+            
+            s = s+1;
+        end
     end
 end
 
