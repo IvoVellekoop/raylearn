142c43b8f3517be1806cb417b7c63cdf731aa167
Daniel <danielcox16@hotmail.com>
2021-02-22 15:34:30 +0100

    Update values, fewer plot updates for fast scan, minor refactor

--- experiments-tpm/calibrate_galvo_fov_aperture.m
+++ experiments-tpm/calibrate_galvo_fov_aperture.m
@@ -8,25 +8,26 @@ if doreset
 end
 
 %% Settings
-doshowcams = 0;                     % Toggle show what the cameras see
+doshowcams = 1;                     % Toggle show what the cameras see
 
 % SLM Settings
-ppp = -12;                          % Pixels per period on SLM grating
+ppp = 3;                            % Pixels per period on SLM grating
 bg_patch_id = 2;                    % Background grating Patch ID
 
 % Galvo Mirror settings
-p.GalvoXcenter = single(-0.11);     % Galvo center x
-p.GalvoYcenter = single(0.04);      % Galvo center y
-p.GalvoXmax    = single(0.08);      % Galvo center to outer, x
-p.GalvoYmax    = single(0.04);      % Galvo center to outer, y
-p.GalvoNX      = single(64);        % Number of Galvo steps, x
-p.GalvoNY      = single(32);        % Number of Galvo steps, y
+p.GalvoXcenter = single(0.00);      % Galvo center x
+p.GalvoYcenter = single(0.00);      % Galvo center y
+p.GalvoXmax    = single(0.70);      % Galvo center to outer, x
+p.GalvoYmax    = single(0.25);      % Galvo center to outer, y
+p.GalvoNX      = single(80);       % Number of Galvo steps, x
+p.GalvoNY      = single(30);        % Number of Galvo steps, y
 
 %% Initialization
 % Set background grating
 [NxSLM, NySLM] = size(slm.getPixels);
+slm_pattern = bg_grating('blaze', ppp, 0, 255, NySLM);
 slm.setRect(bg_patch_id, [0 0 1 1]);
-slm.setData(bg_patch_id, bg_grating('blaze', ppp, 0, 255, NySLM)');
+slm.setData(bg_patch_id, slm_pattern);
 slm.update
 
 % Create set of Galvo tilts
@@ -41,6 +42,12 @@ frames_ft = zeros(copt_img.Width, copt_img.Height, G, 'single');
 %% Loop over galvo tilts
 starttime = now;
 g = 1;
+update_plot_every = 35;
+
+if doshowcams
+    fig_galvoscan = figure;
+    fig_resize(350,3)
+end
 
 for gx = 1:p.GalvoNX                        % Loop over Galvo tilts
     for gy = 1:p.GalvoNY                    % Loop over Galvo tilts
@@ -54,8 +61,8 @@ for gx = 1:p.GalvoNX                        % Loop over Galvo tilts
         galvoscan(gx, gy) = mean(frame_ft, 'all');
 
         % Show what's on the cameras and check for overexposure
-        if doshowcams
-            figure(1)
+        if doshowcams && ~mod(g, update_plot_every)
+            figure(fig_galvoscan)
             subplot(1,2,1)
             imagesc(frame_ft)
             saturation_ft = 100 * max(frame_ft,[],'all') / 65520;   % 12bit -> 2^16 - 2^4 = 65520
@@ -81,6 +88,8 @@ for gx = 1:p.GalvoNX                        % Loop over Galvo tilts
         g = g+1;
     end
 end
+
+outputSingleScan(daqs, [0, 0]);   % Set Galvo Mirror
 
 %% Plot end result
 figure(2)
