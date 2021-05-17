3dd1757123790dad21da8f253a011bd4736ce0e8
Daniel Cox <danielcox16@hotmail.com>
2021-05-17 19:52:48 +0200

    Add to-do mind map

---
+++ todo.mm
@@ -0,0 +1,110 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Correct system&#xa;aberrations in&#xa;experiment and&#xa;measure improvement - 20" FOLDED="false" ID="ID_1454915667" CREATED="1620382095375" MODIFIED="1620389150497" STYLE="oval">
+<font SIZE="10"/>
+<hook NAME="MapStyle" background="#3c3f41" zoom="1.5">
+    <properties edgeColorConfiguration="#808080ff,#00ddddff,#dddd00ff,#dd0000ff,#00dd00ff,#dd0000ff,#7cddddff,#dddd7cff,#dd7cddff,#7cdd7cff,#dd7c7cff,#7c7cddff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#cccccc" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#cccccc" BACKGROUND_COLOR="#3c3f41" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#dddddd" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ff3300">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffb439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#99ffff">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#bbbbbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Compute raylearn&#xa;correction pattern - 8" POSITION="right" ID="ID_1857496707" CREATED="1620385771052" MODIFIED="1620388571469" HGAP_QUANTITY="15.499999955296518 pt" VSHIFT_QUANTITY="-14.999999552965178 pt">
+<edge COLOR="#00dddd"/>
+<node TEXT="Fit system model&#xa;to measurements - 13" ID="ID_1506468163" CREATED="1620385816196" MODIFIED="1620389179584" HGAP_QUANTITY="11.750000067055224 pt" VSHIFT_QUANTITY="-34.499998971819906 pt">
+<node TEXT="Measurements of&#xa;pencil beams&#xa;through grid target - 20" ID="ID_1505952362" CREATED="1620389032328" MODIFIED="1620389249706">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="Repeat with higher res&#xa;and better focus - 13" ID="ID_1731513296" CREATED="1620385827094" MODIFIED="1620389191631"/>
+<node TEXT="Extract positions - 8" ID="ID_1058887996" CREATED="1620385848332" MODIFIED="1620388455116"/>
+<node TEXT="Extract intensities - 8" ID="ID_278351410" CREATED="1620385854155" MODIFIED="1620388449284"/>
+<node TEXT="Convert measurements&#xa;to useable format - 10" ID="ID_1054617586" CREATED="1620385876892" MODIFIED="1620388551453" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="-13.49999959766866 pt"/>
+<node TEXT="Create system model - 13" ID="ID_337365993" CREATED="1620385896214" MODIFIED="1620389205467" HGAP_QUANTITY="12.500000044703482 pt" VSHIFT_QUANTITY="-14.999999552965178 pt">
+<node TEXT="Intensity property - 10" ID="ID_639552022" CREATED="1620386210528" MODIFIED="1620386892010">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="Learn intensity mask shift - 8" ID="ID_1889360279" CREATED="1620386221709" MODIFIED="1620386951487">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="Write unit tests optical - 5" ID="ID_763474568" CREATED="1620636616823" MODIFIED="1621262350873">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="Finish point-/collimated source - 5" ID="ID_903803054" CREATED="1620636635839" MODIFIED="1620664256664">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="Port Snell&apos;s law - 5" ID="ID_260045836" CREATED="1620385908369" MODIFIED="1621262324815">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="Create rotate operation - 10" ID="ID_1282532868" CREATED="1620385919171" MODIFIED="1621262339970">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="Grid mask function&#xa;with convolution - 13" ID="ID_1206566708" CREATED="1620386137921" MODIFIED="1620397770894"/>
+<node TEXT="Create Galvo tilt element - 8" ID="ID_1884701494" CREATED="1620386319285" MODIFIED="1620388794774"/>
+<node TEXT="Create SLM position source - 10" ID="ID_522349803" CREATED="1620386328468" MODIFIED="1620388801782"/>
+<node TEXT="Change vector dim to trailing dim - 8" ID="ID_1842823208" CREATED="1621262396406" MODIFIED="1621262405040"/>
+</node>
+</node>
+<node TEXT="Ray pattern with&#xa;pathlengths to&#xa;correction pattern - 10" ID="ID_1149218589" CREATED="1620386359853" MODIFIED="1620388974595" HGAP_QUANTITY="15.499999955296516 pt" VSHIFT_QUANTITY="-21.749999351799506 pt">
+<node TEXT="pathlength lens function - 10" ID="ID_1603831690" CREATED="1620386389365" MODIFIED="1620388578467" VSHIFT_QUANTITY="34.499998971819906 pt"/>
+<node TEXT="sum of lengths of&#xa;rays =&gt; pathlength - 8" ID="ID_1473567541" CREATED="1620386396085" MODIFIED="1620388712978" VSHIFT_QUANTITY="-3.7499998882412946 pt"/>
+<node TEXT="pathlength to field - 5" ID="ID_1586355190" CREATED="1620386413895" MODIFIED="1620389210045"/>
+<node TEXT="linear interpolation integrator&#xa;/ monte carlo integrator - 20" ID="ID_992576579" CREATED="1620386424687" MODIFIED="1620389214430" HGAP_QUANTITY="12.500000044703482 pt" VSHIFT_QUANTITY="-17.999999463558215 pt"/>
+</node>
+</node>
+</node>
+</map>
