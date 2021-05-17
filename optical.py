f5fe58d0fb7d35161542984d05be6c648c3634a5
Daniel Cox <danielcox16@hotmail.com>
2021-05-17 17:06:47 +0200

    Add Snell's law and smooth grid mask function to optical

--- optical.py
+++ optical.py
@@ -76,7 +76,7 @@ def ideal_lens(in_ray, lens, f):
 
     """
     OC       = lens.position_m                            # Optical Center position
-    BFP      = Plane(OC - f*lens.normal, lens.normal)     # Back Focal Plane
+    BFP      = Plane(OC - f * lens.normal, lens.normal)   # Back Focal Plane
     P        = in_ray.intersect_plane(lens).position_m    # Ray intersection with lens plane
     chiefray = Ray(OC, in_ray.direction)                  # Chief or Principal Ray (through OC)
     focus    = chiefray.intersect_plane(BFP).position_m   # Ray intersection with Back Focal Plane
@@ -89,3 +89,58 @@ def ideal_lens(in_ray, lens, f):
 
     out_ray = Ray(P, unit(focus - P))                     # Output Ray
     return out_ray
+
+
+def smooth_grid(x, y, spacing_x, spacing_y, power):
+    """Smooth Grid function.
+
+    Input
+    -----
+    """
+    cosinegrid, indices = torch.max(torch.stack((
+        (0.5 * torch.cos(x * 2 * np.pi / spacing_x) + 0.5),
+        (0.5 * torch.cos(y * 2 * np.pi / spacing_y) + 0.5))), dim=0)
+
+    return 1 - cosinegrid ** power
+
+
+def intensity_mask_smooth_grid(in_ray, coordplane, spacing_x, spacing_y, power):
+    """Intensity Mask of Smooth Grid.
+
+    Pass rays through intensity mask.
+    """
+    rays_at_plane = in_ray.intersect_plane(coordplane)
+    x, y = coordplane.transform(rays_at_plane)
+    factors = smooth_grid(x, y, spacing_x, spacing_y, power)
+    return rays_at_plane.mask(factors)
+
+
+def snells(ray_in, N, n_out):
+    """
+    Compute refracted Ray. Returns a new Ray instance with the new direction
+    and refractive index.
+
+    Input
+    -----
+        ray_in      Ray. Incoming Ray.
+        N           Unit vector. Surface normal of interface.
+        n_out       Refractive index outgoing ray
+
+    Output
+    ------
+        ray_out     Ray. Refracted outgoing Ray.
+    
+    Notes on derivation: rejection(ray_dir, surface_normal) has magnitude sin(angle)
+    and thus it can act as sine in Snell's law. Furthermore, it is perpendicular to
+    the surface_normal. Hence, it's the perpendicular component of dir_out. Once
+    this is known, the parallel component can also be computed, since |dir_out| = 1.
+    """
+    dir_in = ray_in.direction
+    n_in = ray_in.refractive_index
+
+    dir_inrej = rejection(dir_in, N)       # Perpendicular component dir_in
+    dir_outrej = n_in/n_out * dir_inrej    # Perpendicular component dir_out
+    dir_out = dir_outrej - N * torch.sqrt(1 - norm_square(dir_outrej))
+
+    ray_out = ray_in.copy(direction=dir_out, refractive_index=n_out)
+    return ray_out
