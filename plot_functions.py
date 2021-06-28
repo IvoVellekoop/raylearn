d7fb138c33a8013dce028848938d3794b9b9afbf
Daniel Cox <danielcox16@hotmail.com>
2021-06-28 14:22:24 +0200

    Plot regular planes as well as coordplanes

--- plot_functions.py
+++ plot_functions.py
@@ -4,7 +4,7 @@ import torch
 from torch import tensor, stack
 import numpy as np
 from vector_functions import unit, cross, rejection
-from ray_plane import CoordPlane
+from ray_plane import Plane, CoordPlane
 
 
 def ray_positions(raylist):
@@ -46,16 +46,20 @@ def plot_coords(ax, coords, plotkwargs={'color': 'tab:blue'}):
     return ln
 
 
-def plot_plane(ax, coordplane, scale=1, text='', plotkwargs={'color': 'black'}):
+def plot_plane(ax, plane, scale=1, text='', plotkwargs={'color': 'black'}):
     """Plot a plane.
 
     WIP: currently assumes: dimhori = 2, dimvert = 1, text at point B.
     Improvement: Choose a plotting plane
     """
     # Get properties
-    position_m = coordplane.position_m
-    x = scale * coordplane.x
-    y = scale * coordplane.y
+    position_m = plane.position_m.detach()
+    if isinstance(plane, CoordPlane):
+        x = scale * plane.x.detach()
+        y = scale * plane.y.detach()
+    elif isinstance(plane, Plane):
+        x = scale * unit(rejection(tensor((1., 0., 0.)), plane.normal.detach()))
+        y = scale * unit(rejection(tensor((0., 1., 0.)), plane.normal.detach()))
 
     # Compute 4 corner points of plane
     A = position_m + x + y
@@ -81,7 +85,7 @@ def plot_lens(ax, plane, f, scale, pretext='', plotkwargs={'color': 'black'}):
     y_plane = cross(plane.normal, x_plane)
 
     tilt = np.arcsin(float(plane.normal.unbind(-1)[1].detach())) * 180/np.pi
-    text = pretext + f' f={f*1e3:.1f}mm'
+    text = pretext + f' f={f*1e3:.2f}mm'
     coordplane = CoordPlane(plane.position_m, x_plane, y_plane)
     ln = plot_plane(ax, coordplane, scale, text, plotkwargs)
     return ln
