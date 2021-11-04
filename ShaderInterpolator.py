ec498f1d0b930f53231ee6cc3bdaa4578c9c0dfd
Reinout Nonhebel <github@rpnonhebel.nl>
2021-11-04 18:00:59 +0100

    Add comments to shader interpolation

--- ShaderInterpolator.py
+++ ShaderInterpolator.py
@@ -5,23 +5,30 @@ from OpenGL.GL import arrays
 import numpy as np
 
 def calculate_elements(nx, ny):
-        elements = np.zeros((nx-1)*(ny-1)*6,np.uint32)
-        index = 0
-        for r in range(0, ny-1):
-            for c in range(0,nx-1):
-                base = c + r*(nx)
-                elements[index] = base + 0
-                elements[index + 1] = base + 1
-                elements[index + 2] = base + nx
+    """
+    Helper function for lazy triangulation of a grid.
+    Returns indices of triangle corners
+    """
+    elements = np.zeros((nx-1)*(ny-1)*6,np.uint32)
+    index = 0
+    for r in range(0, ny-1):
+        for c in range(0,nx-1):
+            base = c + r*(nx)
+            elements[index] = base + 0
+            elements[index + 1] = base + 1
+            elements[index + 2] = base + nx
 
-                elements[index + 3] = base + 1
-                elements[index + 4] = base + nx
-                elements[index + 5] = base + nx + 1
-                index += 6
-        return elements
+            elements[index + 3] = base + 1
+            elements[index + 4] = base + nx
+            elements[index + 5] = base + nx + 1
+            index += 6
+    return elements
 
-class shadertest:
-
+class ShaderInterpolator:
+    """
+    Interpolate path length on the GPU through a shader.
+    """
+    
     def load_shaders(self): 
         vertexsource = """
         #version 440 core
@@ -52,12 +59,23 @@ class shadertest:
         self.program = GL.shaders.compileProgram(vs,fs)
 
     def load_data(self, data):
+        """
+        Load data into GPU buffers. `data` is Ny*Nx*3, paired as (y,x,pathlength)
+        Two buffers are created, `vbo` with the raw data, 
+        and `ibo` with the elements (indices) for the triangulation
+        """
         self.nx = data.shape[1]
         self.ny = data.shape[0]
+        
+        # Mandatory for OpenGL to work
         self.vao = GL.glGenVertexArrays(1)
         GL.glBindVertexArray(self.vao)
-        data[:,:,0:2] /= 100e-6
+
+        data[:,:,0:2] /= 100e-6 # TODO make display range adjustable
+        meanpath = np.mean(data[:,:,2])
+        data[:,:,2] -= meanpath
         self.vbo = GL.arrays.vbo.VBO(data)
+        
         elements = calculate_elements(self.nx,self.ny)
         self.ibo = GL.arrays.vbo.VBO(elements, target=GL.GL_ELEMENT_ARRAY_BUFFER)
         
@@ -66,25 +84,38 @@ class shadertest:
         self.vbo.bind()
         self.ibo.bind()
         GL.glUseProgram(self.program)
-        GL.glEnableVertexAttribArray(0)
-        GL.glEnableVertexAttribArray(1)
+        GL.glEnableVertexAttribArray(0) # input 0: vertexPosition_modelspace 
+        GL.glEnableVertexAttribArray(1) # input 1: vertexColor
         
+        # Position. Two doubles, stride 3*8 bytes
         GL.glVertexAttribPointer(0, 2, GL.GL_DOUBLE, GL.GL_FALSE, 24, self.vbo)
+        # Pathlength/'color'. One double, stride 3*8 bytes, offset 2*8 bytes
         GL.glVertexAttribPointer(1, 1, GL.GL_DOUBLE, GL.GL_FALSE, 24, self.vbo+16)
         
         GL.glDrawElements(GL.GL_TRIANGLES, (self.nx - 1)*(self.ny - 1)*6, GL.GL_UNSIGNED_INT, None)
         GLUT.glutSwapBuffers()
 
     def __init__(self, data):
+        # Create OpenGL context
         GLUT.glutInit()
         GLUT.glutInitContextVersion(4,4)
         GLUT.glutInitContextProfile(GLUT.GLUT_CORE_PROFILE)
         GLUT.glutInitWindowPosition(100,100)
         GLUT.glutInitWindowSize(600,600)
         window = GLUT.glutCreateWindow("A window!")
+        
+        # Callback function to run each frame
         GLUT.glutDisplayFunc(self.draw_frame)
+        
+        # Prepare GPU resources
         self.load_shaders()
         self.load_data(data)
+
+        # Enable blending for addition of fields
         GL.glClearColor(0, 0.5, 0, 1)
-        GL.glPointSize(5)
+        GL.glEnable(GL.GL_BLEND) 
+        GL.glBlendFunc(GL.GL_ONE, GL.GL_ONE)
+        # Default blendEquation is already GL_ADD
+        #GL.glBlendEquation(GL.GL_ADD)
+        
         GLUT.glutMainLoop()
--- try_field.py
+++ try_field.py
@@ -15,6 +15,10 @@ from vector_functions import cartesian3d
 torch.set_default_tensor_type('torch.DoubleTensor')
 plt.rc('font', size=12)
 
+""""
+This is a playground to test interpolation functions
+"""
+
 class FocusingSystem():
 
     def __init__(self):
@@ -185,32 +189,37 @@ plt.show()
 planepts = 600
 max_size = 75e-6
 
-field_coords = coord_grid(limits=(-max_size, max_size, -max_size,max_size), resolution=(planepts,planepts))
+# field_coords = coord_grid(limits=(-max_size, max_size, -max_size,max_size), resolution=(planepts,planepts))
 
-field_out = torch.empty(planepts,planepts,1, dtype=torch.cfloat)
-for i,row in enumerate(field_coords):
-    field_row = field_from_rays(system.rays[-1], system.cam_im_plane, row.unsqueeze(0))
-    field_out[i] = field_row
-    if np.mod(i,10) == 0: # display progress
-        print(i)
+# # Loop rows to interpolate larger sets of rays (very slow)
+# field_out = torch.empty(planepts,planepts,1, dtype=torch.cfloat)
+# for i,row in enumerate(field_coords):
+#     field_row = field_from_rays(system.rays[-1], system.cam_im_plane, row.unsqueeze(0))
+#     field_out[i] = field_row
+#     if np.mod(i,10) == 0: # display progress
+#         print("%.1f%%"%(i/field_coords.shape[0] * 100))
 
-# Interpolate the whole field at once, uses too much memory
+# # Interpolate the whole field at once, uses too much memory
 # field_out = field_from_rays(system.rays[-1], system.cam_im_plane, field_coords)
 
-fig = plt.figure(figsize=(5, 4))
-fig.dpi = 144
-ax1 = plt.gca()
-plt.imshow(torch.angle(field_out), extent=(-max_size, max_size, -max_size, max_size), interpolation='none', origin='lower')
-plt.colorbar()
-plt.title('Field at camera plane')
-plt.xlabel('x [m]')
-plt.ylabel('y [m]')
-plt.show()
+# # Display interpolated field
+# fig = plt.figure(figsize=(5, 4))
+# fig.dpi = 144
+# ax1 = plt.gca()
+# plt.imshow(torch.angle(field_out), extent=(-max_size, max_size, -max_size, max_size), interpolation='none', origin='lower')
+# plt.colorbar()
+# plt.title('Field at camera plane')
+# plt.xlabel('x [m]')
+# plt.ylabel('y [m]')
+# plt.show()
 
-from scipy.io import savemat
+# # Save interpolated field to file
+# from scipy.io import savemat
 # savemat('outfield11.mat', mdict={'field_out': field_out.numpy()})
+
+# Try interpolation in a shader (very fast)
 pos = cam_coords
 path = system.rays[-1].pathlength_m
 data = torch.cat((pos,path),2)
-from shadertest import shadertest
-t = shadertest(data.numpy())
+from ShaderInterpolator import ShaderInterpolator
+t = ShaderInterpolator(data.numpy())
