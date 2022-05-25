217dfbac63d3ff11d80169318184b69d0cc92c47
Daniel Cox <danielcox16@hotmail.com>
2022-05-25 18:21:01 +0200

    Include img cam in galvo FOV scan

--- experiments-tpm/calibrate_galvo_fov_aperture.m
+++ experiments-tpm/calibrate_galvo_fov_aperture.m
@@ -2,7 +2,7 @@
 
 doreset = 0;
 
-if doreset || ~exist('slm', 'var') || ~exist('daqs', 'var')  || ~exist('cam_ft', 'var')
+if doreset || ~exist('slm', 'var') || ~exist('daqs', 'var')  || ~exist('cam_ft', 'var')  || ~exist('cam_img', 'var')
     close all; clear; clc
     setup_raylearn_exptpm
 end
@@ -17,10 +17,10 @@ bg_patch_id = 2;                    % Background grating Patch ID
 % Galvo Mirror settings
 p.GalvoXcenter = single(0.00);      % Galvo center x
 p.GalvoYcenter = single(0.00);      % Galvo center y
-p.GalvoXmax    = single(1.25);      % Galvo center to outer, x
-p.GalvoYmax    = single(0.40);      % Galvo center to outer, y
-p.GalvoNX      = single(80);        % Number of Galvo steps, x
-p.GalvoNY      = single(25);        % Number of Galvo steps, y
+p.GalvoXmax    = single(1.00);      % Galvo center to outer, x
+p.GalvoYmax    = single(0.30);      % Galvo center to outer, y
+p.GalvoNX      = single(100);       % Number of Galvo steps, x
+p.GalvoNY      = single(30);        % Number of Galvo steps, y
 
 %% Initialization
 % Set background grating
@@ -34,9 +34,11 @@ slm.update
 p.galvoXs1d = linspace(p.GalvoXcenter-p.GalvoXmax, p.GalvoXcenter+p.GalvoXmax, p.GalvoNX);
 p.galvoYs1d = linspace(p.GalvoYcenter-p.GalvoYmax, p.GalvoYcenter+p.GalvoYmax, p.GalvoNY);
 G = p.GalvoNX * p.GalvoNY;
-galvoscan = zeros(p.GalvoNX, p.GalvoNY);
+galvoscan_ft  = zeros(p.GalvoNX, p.GalvoNY);
+galvoscan_img = zeros(p.GalvoNX, p.GalvoNY);
 
-frames_ft = zeros(copt_img.Width, copt_img.Height, G, 'single');
+frames_ft  = zeros(copt_ft.Width,  copt_ft.Height,  G, 'single');
+frames_img = zeros(copt_img.Width, copt_img.Height, G, 'single');
 
 
 %% Loop over galvo tilts
@@ -55,28 +57,41 @@ for gx = 1:p.GalvoNX                        % Loop over Galvo tilts
         outputSingleScan(daqs, [p.galvoXs1d(gx), p.galvoYs1d(gy)]);   % Set Galvo Mirror
         pause(0.005)
 
-        % Capture camera frames and mean as scanpixel
+        % Capture fourier camera frames and mean as scanpixel
         cam_ft.trigger;
         frame_ft = single(cam_ft.getData);
         frames_ft(:,:,g) = frame_ft;
-        galvoscan(gx, gy) = mean(frame_ft, 'all');
+        galvoscan_ft(gx, gy) = mean(frame_ft, 'all');
+
+        % Capture image camera frames and max as scanpixel
+        cam_img.trigger;
+        frame_img = single(cam_img.getData);
+        frames_img(:,:,g) = frame_img;
+        galvoscan_img(gx, gy) = max(frame_img, [], 'all');
 
         % Show what's on the cameras and check for overexposure
         if doshowcams && ~mod(g, update_plot_every)
             figure(fig_galvoscan)
             subplot(1,2,1)
-            imagesc(frame_ft)
-            saturation_ft = 100 * max(frame_ft,[],'all') / 65520;   % 12bit -> 2^16 - 2^4 = 65520
-            title(sprintf('Fourier Plane Cam | Tilt %i/%i\nsaturation = %.0f%%', g, G, saturation_ft))
+            frame_ft_normalized  = frame_ft  ./ max(frame_img, [], [1 2]);
+            frame_img_normalized = frame_img ./ max(frame_img, [], [1 2]);
+            color_coded_frame = cat(3, frame_img_normalized, frame_ft_normalized, frame_ft_normalized);
+            imagesc(color_coded_frame);
+            saturation_img = 100 * max(frame_img, [], 'all') / 65520;   % 12bit -> 2^16 - 2^4 = 65520
+            title(sprintf('Red=img cam, Cyan=Fourier cam | Tilt %i/%i\nsaturation = %.0f%%', g, G, saturation_img))
             axis image
             colorbar
 
             subplot(1,2,2)
-            imagesc(sqrt(galvoscan'), 'XData', p.galvoXs1d, 'YData', p.galvoYs1d)
+            galvoscan_ft_normalized  = galvoscan_ft  ./ max(galvoscan_ft,  [], [1 2]);
+            galvoscan_img_normalized = galvoscan_img ./ max(galvoscan_img, [], [1 2]);
+            galvoscan_color_coded_normalized = cat(3, galvoscan_img_normalized, ...
+                galvoscan_ft_normalized, galvoscan_ft_normalized);
+            imagesc(permute(sqrt(galvoscan_color_coded_normalized), [2 1 3]), 'XData', p.galvoXs1d, 'YData', p.galvoYs1d)
             hold on
             plot(p.galvoXs1d(gx), p.galvoYs1d(gy), 'or')
             hold off
-            title('Galvo Scan sqrt(Mean Intensity)')
+            title(sprintf('Galvo Scan sqrt(Mean Intensity)\nRed=img cam, Cyan=Fourier cam'))
             xlabel('Galvo voltage X')
             ylabel('Galvo voltage Y')
             axis image
@@ -94,7 +109,11 @@ outputSingleScan(daqs, [0, 0]);   % Set Galvo Mirror
 
 %% Plot end result
 figure(2)
-imagesc(sqrt(galvoscan'), 'XData', p.galvoXs1d, 'YData', p.galvoYs1d)
+            galvoscan_ft_normalized  = galvoscan_ft  ./ max(galvoscan_ft,  [], [1 2]);
+            galvoscan_img_normalized = galvoscan_img ./ max(galvoscan_img, [], [1 2]);
+            galvoscan_color_coded_normalized = cat(3, galvoscan_img_normalized, ...
+                galvoscan_ft_normalized, galvoscan_ft_normalized);
+            imagesc(permute(sqrt(galvoscan_color_coded_normalized), [2 1 3]), 'XData', p.galvoXs1d, 'YData', p.galvoYs1d)
 title(sprintf('Galvo Scan sqrt(Mean Intensity) | ppp=%.2f', ppp))
 xlabel('Galvo voltage X')
 ylabel('Galvo voltage Y')
