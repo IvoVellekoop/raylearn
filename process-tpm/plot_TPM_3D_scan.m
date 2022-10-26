4b445e541786d2206ee81f7652d5cede0f50b0e8
Daniel Cox <danielcox16@hotmail.com>
2022-10-26 16:10:27 +0200

    Load and plot TPM 3D scan

---
+++ process-tpm/plot_TPM_3D_scan.m
@@ -0,0 +1,80 @@
+clear; close all; clc
+
+% Plot TPM 3D scan
+forcereset = 0;
+
+if forcereset || ~exist('dirs', 'var')
+    close all; clear; clc
+
+    % Define location paths
+    addpath(fileparts(fileparts(mfilename('fullpath'))))   % Parent dir
+    dirconfig_raylearn
+end
+
+%%
+% BMPI/Projects/WAVEFRONTSHAPING/data/TPM/4th gen/calibration/calibration_values.mat
+ %%%%% Automate: M matrix, pixels in frame / pixels per line, zoom
+pixels_per_um = 0.2033 * 4 * 30;
+FOV_um = 1/pixels_per_um * 1024;
+zstep_um = 0.4;
+tifpath = "/home/dani/LocalData/raylearn-data/TPM/TPM-3D-scans/beads-0.5um-in-25uL-cyl-zoom30-zstep0.4um_00002.tif";
+
+framestack = [];
+n = 1;
+disp('')
+numchars = 0;
+while true
+    try
+        framestack(:,:,n) = imread(tifpath, n);
+    catch err
+        if strcmp(err.identifier, 'MATLAB:imagesci:rtifc:invalidDirIndex')
+            break;
+        else
+            error(err.message)
+        end
+    end
+    fprintf(repmat('\b', [1 numchars]))
+    numchars = fprintf('Loading frame %i\n', n);
+    
+    n = n+1;
+end
+
+framestack(framestack<0) = 0;
+
+stack_depth_um = zstep_um * size(framestack, 3);
+
+%% Plot
+um = [' (' 181 'm)'];
+fig = figure;
+dimdata = {[-FOV_um/2 FOV_um/2], [-FOV_um/2 FOV_um/2], [-stack_depth_um/2 stack_depth_um/2]};
+axesoptions = struct('xlim', [-4 8], 'ylim', [-8 4], 'fontsize', 14);
+
+% Plot with im3
+im3(framestack, ...
+    'title', ['0.5' 181 'm beads in 25' 181 'L tube (cropped)'], ...
+    'dimdata', dimdata, ...
+    'dimlabels', {['x' um], ['y' um], ['z' um]}, ...
+    'axesoptions', axesoptions)
+colormap inferno
+
+fig_resize(450, 1.05);
+savename = ['plots/0.5' 181 'm-beads-in-25' 181 'L-tube-cropped-slice%03i.png'];
+
+fig.UserData.findmax(fig)
+
+%% Save plots of a few different slices
+fig.UserData.changeslice(fig, -8)
+ax = gca;
+
+for s = 1:6
+    saveas(fig, sprintf(savename, fig.UserData.slicenum))
+    fig.UserData.changeslice(fig, 4)
+    
+    zposition_um = zstep_um * fig.UserData.slicenum - stack_depth_um/2;
+    subtitle = ax.Title.String{2,1};
+    subtitle = sprintf(['%s, z=%.2f' 181 'm'], subtitle, zposition_um);
+    ax.Title.String{2,1} = subtitle;
+end
+
+
+
