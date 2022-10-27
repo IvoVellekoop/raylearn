35d3011823478c9ee94f0403d85df07594bf1310
Daniel Cox <danielcox16@hotmail.com>
2022-10-27 15:25:35 +0200

    Plot different 3D TPM scan

--- process-tpm/plot_TPM_3D_scan.m
+++ process-tpm/plot_TPM_3D_scan.m
@@ -13,12 +13,22 @@ end
 
 %%
 % BMPI/Projects/WAVEFRONTSHAPING/data/TPM/4th gen/calibration/calibration_values.mat
- %%%%% Automate: M matrix, pixels in frame / pixels per line, zoom
-pixels_per_um = 0.2033 * 4 * 30;
-FOV_um = 1/pixels_per_um * 1024;
-zstep_um = 0.4;
-tifpath = "/home/dani/LocalData/raylearn-data/TPM/TPM-3D-scans/beads-0.5um-in-25uL-cyl-zoom30-zstep0.4um_00002.tif";
+%%%%% Automate: M matrix, pixels in frame / pixels per line, zoom
 
+% tifpath = "/home/dani/LocalData/raylearn-data/TPM/TPM-3D-scans/beads-0.5um-in-25uL-cyl-zoom30-zstep0.4um_00002.tif";
+% zoom = 30;
+% factor = 4;
+% zstep_um = 0.4;
+
+tifpath = "/home/dani/LocalData/raylearn-data/TPM/TPM-3D-scans/beads-0.5um-in-25uL-cyl-zoom15-zstep0.5_00001.tif";
+zoom = 15;
+factor = 4;
+zstep_um = 0.5;
+pixels_per_um = 0.2033 * factor * zoom;
+
+FOV_um = 1/pixels_per_um * 1024;
+
+%% Load frames
 framestack = [];
 n = 1;
 disp('')
@@ -47,7 +57,8 @@ stack_depth_um = zstep_um * size(framestack, 3);
 um = [' (' 181 'm)'];
 fig = figure;
 dimdata = {[-FOV_um/2 FOV_um/2], [-FOV_um/2 FOV_um/2], [-stack_depth_um/2 stack_depth_um/2]};
-axesoptions = struct('xlim', [-4 8], 'ylim', [-8 4], 'fontsize', 14);
+% axesoptions = struct('xlim', [-4 8], 'ylim', [-8 4], 'fontsize', 14);
+axesoptions = struct('fontsize', 14);
 
 % Plot with im3
 im3(framestack, ...
@@ -57,18 +68,18 @@ im3(framestack, ...
     'axesoptions', axesoptions)
 colormap inferno
 
-fig_resize(450, 1.05);
+fig_resize(650, 1.05);
 savename = ['plots/0.5' 181 'm-beads-in-25' 181 'L-tube-cropped-slice%03i.png'];
 
-fig.UserData.findmax(fig)
+fig.UserData.slicenum = 21;
 
 %% Save plots of a few different slices
-fig.UserData.changeslice(fig, -8)
+fig.UserData.changeslice(fig, 0)
 ax = gca;
 
-for s = 1:6
+for s = 1:1
     saveas(fig, sprintf(savename, fig.UserData.slicenum))
-    fig.UserData.changeslice(fig, 4)
+    fig.UserData.changeslice(fig, 0)
     
     zposition_um = zstep_um * fig.UserData.slicenum - stack_depth_um/2;
     subtitle = ax.Title.String{2,1};
