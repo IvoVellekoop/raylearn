e139deba903f5cdefb534bd0102955474dfebf7c
Daniel Cox <danielcox16@hotmail.com>
2023-05-12 10:26:38 +0200

    Change width into std

--- smooth-snells-law-test.py
+++ smooth-snells-law-test.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from vector_functions import ensure_tensor
 
 
-torch.set_default_tensor_type('torch.DoubleTensor')
+# torch.set_default_tensor_type('torch.DoubleTensor')
 
 
 def snells(Dxin, n1, n2):
@@ -51,14 +51,14 @@ def smooth_snells_lorentzian_2d(DxInCenter, width, n1, n2):
             arccot((n1*width)/(DxInCenter*n1 + n2))))
 
 
-def smooth_snells(DxInCenter, width, n1, n2, epsilon=0):
+def smooth_snells(DxInCenter, std, n1, n2):
     """
     Smoothed Snell's law, with gaussian distribution as input.
 
     Input
     -----
     DxInCenter
-    width       Standard deviation (sigma) of gaussian distribution in k_xy plane.
+    std       Standard deviation (sigma) of gaussian distribution in k_xy plane.
     """
     # DxInCrit = ensure_tensor(n2/n1)
     # intensity = (torch.erf((DxInCrit - DxInCenter) / (np.sqrt(2)*width)) +
@@ -68,41 +68,38 @@ def smooth_snells(DxInCenter, width, n1, n2, epsilon=0):
     # return DxOutMean
 
     DxInCrit = ensure_tensor(n2/n1)
-    intensity = (torch.erf((DxInCrit - DxInCenter) / (np.sqrt(2)*width)) +
-                 torch.erf((DxInCrit + DxInCenter) / (np.sqrt(2)*width))) / 2
-    DxInMean = DxInCenter + np.sqrt(2/np.pi) * width * (\
+    intensity = (torch.erf((DxInCrit + DxInCenter) / (np.sqrt(2)*std)) +
+                 torch.erf((DxInCrit - DxInCenter) / (np.sqrt(2)*std))) / 2
+    DxInMean = DxInCenter + np.sqrt(2/np.pi) * std * (\
           torch.exp(
-            -((DxInCenter + DxInCrit)**2/(2*width**2)) \
-            - torch.log( \
-                - torch.erf((DxInCenter - DxInCrit)/(np.sqrt(2)*width)) \
-                + torch.erf((DxInCenter + DxInCrit)/(np.sqrt(2)*width)) \
-                + epsilon))
+            -((DxInCenter + DxInCrit)**2/(2*std**2)) \
+            - torch.log(2*intensity))
         - torch.exp(
-            (2*DxInCenter*DxInCrit)/width**2 \
-            - (DxInCenter + DxInCrit)**2/(2*width**2) \
-            - torch.log(
-                - torch.erf((DxInCenter - DxInCrit)/(np.sqrt(2)*width)) \
-                + torch.erf((DxInCenter + DxInCrit)/(np.sqrt(2)*width)) \
-                + epsilon)))
+            (2*DxInCenter*DxInCrit)/std**2 \
+            - (DxInCenter + DxInCrit)**2/(2*std**2) \
+            - torch.log(2*intensity)))
     DxOutMean = DxInMean * n1/n2
     return DxOutMean
 
+    # Check integration limits for n1<n2
     # Compute DzOut
     # Also apply intensity factor to ray
     # Fresnel equations
+    # Add check for extreme exponents with poor precision
 
 
 N = 800
 DxInCenter = torch.linspace(-1, 1, N).view((-1, 1))
 
-width = tensor((0.2, 0.1, 0.05, 0.02, 0.01)).view((1, -1))
+# width = tensor((0.2, 0.1, 0.05, 0.02, 0.01)).view((1, -1))
+stds = tensor((0.0892, 0.04,)).view((1, -1))
 n1 = 1.5
 n2 = 1.3
 
 # Plot DxOut vs DxIn
-widthlabels = ['Width=' + f'{w:.2f}' for w in width.view(-1)]
+widthlabels = ['std=' + f'{w:.3f}' for w in stds.view(-1)]
 plt.plot(DxInCenter, snells(DxInCenter, n1, n2), label="Regular Snell's")
-plt.plot(DxInCenter, smooth_snells(DxInCenter, width, n1, n2), label=widthlabels)
+plt.plot(DxInCenter, smooth_snells(DxInCenter, stds, n1, n2), label=widthlabels)
 
 plt.xlabel('Incoming direction x-component $(k_{x,in}/k_{in})$')
 plt.ylabel('Outgoing direction x-component $(k_{x,out}/k_{out})$')
@@ -115,9 +112,9 @@ plt.show()
 # Plot angle out vs angle in
 theta_xincenter = torch.linspace(-np.pi/2, np.pi/2, N).reshape((-1, 1))
 
-widthlabels = ['Width=' + f'{w:.2f}' for w in width.reshape(-1)]
+widthlabels = ['std=' + f'{w:.3f}' for w in stds.reshape(-1)]
 plt.plot(theta_xincenter, np.arcsin(snells(np.sin(theta_xincenter), n1, n2)), label="Regular Snell's")
-plt.plot(theta_xincenter, np.arcsin(smooth_snells(np.sin(theta_xincenter), width, n1, n2)), label=widthlabels)
+plt.plot(theta_xincenter, np.arcsin(smooth_snells(np.sin(theta_xincenter), stds, n1, n2)), label=widthlabels)
 
 plt.xlabel('$\\theta_{in}$ (rad)')
 plt.ylabel('$\\theta_{out}$ (rad)')
