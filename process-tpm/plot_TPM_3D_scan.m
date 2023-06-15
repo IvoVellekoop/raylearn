b54157bb9003283f9e0c084abac3f376653ad967
Daniel Cox <danielcox16@hotmail.com>
2023-06-15 17:04:37 +0200

    Update settings plot_TPM_3D_scan

--- process-tpm/plot_TPM_3D_scan.m
+++ process-tpm/plot_TPM_3D_scan.m
@@ -12,18 +12,27 @@ if forcereset || ~exist('dirs', 'var')
     addpath(fileparts(fileparts(mfilename('fullpath'))))   % Parent dir
     dirconfig_raylearn
 end
-
 dosave = 0;
 
 %%
 % BMPI/Projects/WAVEFRONTSHAPING/data/TPM/4th gen/calibration/calibration_values.mat
 %%%%% Automate: M matrix, pixels in frame / pixels per line, zoom
 
-% tifpath = fullfile(dirs.expdata, 'raylearn-data/TPM/TPM-3D-scans/2023-03-24_tube-0.5uL-zoom10-no-correction_00003.tif');
-tifpath = fullfile(dirs.expdata, 'raylearn-data/TPM/TPM-3D-scans/2023-03-24_tube-0.5uL-zoom10-with-correction_00001.tif');
-zoom = 10;
-factor = 4;
-zstep_um = 1;
+% tifpath = '/mnt/bmpi/Data/Daniel Cox/ExperimentalData/raylearn-data/TPM/TPM-3D-scans/tube-0.5uL-zoom10-no-correction_00001.tif';
+
+% tifpath = '/mnt/bmpi/Data/Daniel Cox/ExperimentalData/raylearn-data/TPM/TPM-3D-scans/scans-AO-vs-RT-5mW-try2/tube-500nL-zoom9-bottom-AO_00001.tif'; titlestr = 'bottom AO 5mW';
+% tifpath = '/mnt/bmpi/Data/Daniel Cox/ExperimentalData/raylearn-data/TPM/TPM-3D-scans/scans-AO-vs-RT-5mW-try2/tube-500nL-zoom9-bottom-RT_00001.tif'; titlestr = 'bottom RT 5mW';
+% tifpath = '/mnt/bmpi/Data/Daniel Cox/ExperimentalData/raylearn-data/TPM/TPM-3D-scans/scans-AO-vs-RT-5mW-try2/tube-500nL-zoom9-top-AO_00001.tif'; titlestr = 'top AO 5mW';
+% tifpath = '/mnt/bmpi/Data/Daniel Cox/ExperimentalData/raylearn-data/TPM/TPM-3D-scans/scans-AO-vs-RT-5mW-try2/tube-500nL-zoom9-top-RT_00001.tif'; titlestr = 'top RT 5mW';
+% tifpath = '/mnt/bmpi/Data/Daniel Cox/ExperimentalData/raylearn-data/TPM/TPM-3D-scans/scans-AO-vs-RT-5mW-try2/tube-500nL-zoom9-no-correction-1_00001.tif'; titlestr = 'no correction (before) 5mW';
+% tifpath = '/mnt/bmpi/Data/Daniel Cox/ExperimentalData/raylearn-data/TPM/TPM-3D-scans/scans-AO-vs-RT-5mW-try2/tube-500nL-zoom9-no-correction-2_00001.tif'; titlestr = 'no correction (halfway) 5mW';
+tifpath = '/mnt/bmpi/Data/Daniel Cox/ExperimentalData/raylearn-data/TPM/TPM-3D-scans/scans-AO-vs-RT-5mW-try2/tube-500nL-zoom9-no-correction-3_00001.tif'; titlestr = 'no correction (after) 5mW';
+
+% tifpath = "/home/dani/LocalData/raylearn-data/TPM/TPM-3D-scans/beads-0.5um-in-25uL-cyl-zoom30-zstep0.4um_00002.tif";
+% tifpath = "D:\ScientificData\TPM-3D-scans\beads-0.5um-in-25uL-cyl-zoom30-zstep0.4um_00002.tif";
+zoom = 9;
+factor = 1024/256;
+zstep_um = 1.5;
 
 % % tifpath = "/home/dani/LocalData/raylearn-data/TPM/TPM-3D-scans/beads-0.5um-in-25uL-cyl-zoom15-zstep0.5_00001.tif";
 % tifpath = "D:\ScientificData\TPM-3D-scans\beads-0.5um-in-25uL-cyl-zoom15-zstep0.5_00001.tif";
@@ -40,10 +49,8 @@ tifinfo = imfinfo(tifpath);         % Get info about image
 num_of_frames = length(tifinfo);    % Number of frames
 framestack = zeros(tifinfo(1).Width, tifinfo(1).Height, num_of_frames); % Initialize
 
-starttime = now;
 for n = 1:num_of_frames
     framestack(:,:,n) = imread(tifpath, n);
-    eta(n, num_of_frames, starttime, 'console', 'Loading tif...', 5);
 end
 
 
@@ -54,101 +61,37 @@ stack_depth_um = zstep_um * size(framestack, 3);
 %% Plot
 um = [' (' 181 'm)'];
 fig = figure;
-<<<<<<< HEAD
-dimdata = {[-FOV_um/2 FOV_um/2], [-FOV_um/2 FOV_um/2], [stack_depth_um/2 -stack_depth_um/2]};
-=======
 dimdata = {[-FOV_um/2 FOV_um/2], [-FOV_um/2 FOV_um/2], [-stack_depth_um/2 stack_depth_um/2]};
->>>>>>> 94f03aa (Update settings)
 % axesoptions = struct('xlim', [-4 8], 'ylim', [-8 4], 'fontsize', 16);
 axesoptions = struct('fontsize', 14);
 
 % Plot with im3
-<<<<<<< HEAD
-im3(framestack, ...
-    'title', ['0.5' 181 'm beads in 0.5' 181 'L tube'], ...
-    'slicedim', 3, ...
-    'slicenum', 160, ...
-=======
-im3(min(framestack, 15000), ...
-    'title', ['0.5' 181 'm beads in 25' 181 'L tube (cropped)'], ...
->>>>>>> 94f03aa (Update settings)
+im3(log(flip(framestack, 3)), ...
+    'slicedim', 1,...
+    'maxprojection', true,...
+    'title', ['0.5' 181 'm beads in 0.5' 181 'L tube, ' titlestr], ...
     'dimdata', dimdata, ...
-    'dimlabels', {['x' um], ['y' um], ['z' um]}, ...
-    'maxprojection', 0, ...
+    'dimlabels', {['y' um], ['x' um], ['z' um]}, ...
     'axesoptions', axesoptions)
 colormap inferno
 
-pause(0.5)
-
-title(sprintf(['0.5' 181 'm beads in 0.5' 181 'L tube\nslice at z=%.0f' 181 'm'], stack_depth_um/2 - fig.UserData.slicenum * zstep_um))
-
-fig_resize(600, 1.1);
+fig_resize(550, 1.05);
 movegui('center')
-% caxis([0 8e3])
-ylabel(fig.Children(1), 'Intensity')
+savename = ['../plots/0.5' 181 'm-beads-in-25' 181 'L-tube-cropped-slice%03i.png'];
 
-%% Plot
-um = [' (' 181 'm)'];
-fig = figure;
-dimdata = {[-FOV_um/2 FOV_um/2], [-FOV_um/2 FOV_um/2], [stack_depth_um/2 -stack_depth_um/2]};
-% axesoptions = struct('xlim', [-4 8], 'ylim', [-8 4], 'fontsize', 16);
-axesoptions = struct('fontsize', 14);
+fig.UserData.slicenum = 5;
+cb = colorbar;
+ylabel(cb,'log(signal)')
+drawnow
+pause(0.1)
+% title(titlestr)
+caxis([5.5 8.0])
 
-% Plot with im3
-im3(framestack, ...
-    'title', ['0.5' 181 'm beads in 0.5' 181 'L tube'], ...
-    'slicedim', 1, ...
-    'dimdata', dimdata, ...
-    'dimlabels', {['x' um], ['y' um], ['z' um]}, ...
-    'maxprojection', 1, ...
-    'axesoptions', axesoptions)
-colormap inferno
-
-pause(0.5)
-
-title(sprintf(['0.5' 181 'm beads in 0.5' 181 'L tube\nmax intensity projection'], stack_depth_um/2 - fig.UserData.slicenum * zstep_um))
-
-fig_resize(600, 0.9);
-movegui('center')
-% caxis([0 8e3])
-ylabel(fig.Children(1), 'Intensity')
-
-
-%% Determine FWHM
-figure;
-imagesc(framestack(310:370,487:492,164));
-caxis([0 5e3]);
-axis image
-
-figure;
-frameline = mean(framestack_raw(310:370,487:492,164), 2);
-x = (1:length(frameline))/pixels_per_um;
-f = fit(x',frameline,'gauss1');
-
-xfit = linspace(min(x), max(x), 200);
-yfit = f.a1 * exp(-((xfit - f.b1) ./ f.c1).^2);
-plot(x, frameline, '.-'); hold on
-plot(xfit, yfit, 'k')
-xlabel('x (um)')
-set(gca, 'fontsize', 14)
-
-sigma = f.c1 / sqrt(2);
-FWHM = 2*sqrt(2*log(2)) * sigma;
-plot(f.b1 - (FWHM/2 * [-1 1]), f.a1 * [1 1]/2, '.-r')
-hold off
-legend('image line', 'fit', 'FWHM')
-title(sprintf('FWHM = %.3fum', FWHM))
-
-%% Save?
-savename = ['../plots/0.5' 181 'm-beads-in-0.5' 181 'L-tube-cropped-slice%03i.png'];
+%% Save plots of a few different slices
 
 if dosave
-    fig.UserData.slicenum = 5;
-
-    %% Save plots of a few different slices
     fig.UserData.changeslice(fig, 0)
     ax = gca;
-
     for s = 1:6
         zposition_um = zstep_um * fig.UserData.slicenum - stack_depth_um/2;
         subtitle = ax.Title.String{2,1};
