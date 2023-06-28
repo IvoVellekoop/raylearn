30bff95916a6fc1dc89ce2948cc1e6b9728a00f4
Daniel Cox <danielcox16@hotmail.com>
2023-06-28 14:09:43 +0200

    Plot photobleaching from new framestacks

--- process-tpm/plot_photobleaching_from_framestacks.m
+++ process-tpm/plot_photobleaching_from_framestacks.m
@@ -1,33 +1,50 @@
+forcereset = 0;
+
+if forcereset || ~exist('dirs', 'var')
+    close all; clear; clc
+
+    % Define location paths
+    addpath(fileparts(fileparts(mfilename('fullpath'))))   % Parent dir
+    dirconfig_raylearn
+end
+
 %%
 
-framestack_raw_1 = load_framestack('/mnt/bmpi/Data/Daniel Cox/ExperimentalData/raylearn-data/TPM/TPM-3D-scans/scans-AO-vs-RT-5mW-try2/tube-500nL-zoom9-no-correction-1_00001.tif');
-framestack_raw_2 = load_framestack('/mnt/bmpi/Data/Daniel Cox/ExperimentalData/raylearn-data/TPM/TPM-3D-scans/scans-AO-vs-RT-5mW-try2/tube-500nL-zoom9-no-correction-2_00001.tif');
-framestack_raw_3 = load_framestack('/mnt/bmpi/Data/Daniel Cox/ExperimentalData/raylearn-data/TPM/TPM-3D-scans/scans-AO-vs-RT-5mW-try2/tube-500nL-zoom9-no-correction-3_00001.tif');
-framestacks_raw = [framestack_raw_1(:) framestack_raw_2(:) framestack_raw_3(:)];
+tiffolder = '/mnt/bmpi/Data/Daniel Cox/ExperimentalData/raylearn-data/TPM/TPM-3D-scans/23-Jun-2023_tube-500nL/';
 
-%%
+M = 5;  % Number of measurements
 percentiles = 99.991:0.002:99.999;
+high_signals = zeros(M, numel(percentiles));
 
-high_signals = prctile(framestacks_raw, percentiles)';
+for index = 1:M
+    framestack_raw = load_framestack([tiffolder sprintf('tube-500nL-zoom8-zstep1.400um-no-correction-%i_00001.tif', index)], index, M);
+    framestack = framestack_raw - mean(framestack_raw(:));
+    high_signals(index, :) = prctile(framestack(:), percentiles)';
+end
+
 
 %% Plot
 plot(high_signals, '.-');
-xticks([1 2 3])
-xticklabels(["Before", "Halfway", "After"])
+% xticks([1 2 3])
+% xticklabels(["Before", "Halfway", "After"])
 ylabel('Signal')
+ylim([0 max(high_signals(:))])
 title('Percentiles of flat pattern measurements')
+legend({}, 'Location', 'Best')
 legend(strcat(num2str(percentiles'), repmat("%tile", [numel(percentiles) 1])))
 set(gca, 'FontSize', 14)
 
 %%
-function framestack_raw = load_framestack(tifpath)
+function framestack_raw = load_framestack(tifpath, index, M)
     %% Load frames
     tifinfo = imfinfo(tifpath);         % Get info about image
     num_of_frames = length(tifinfo);    % Number of frames
     framestack = zeros(tifinfo(1).Width, tifinfo(1).Height, num_of_frames); % Initialize
     
+    starttime = now;
     for n = 1:num_of_frames
         framestack(:,:,n) = imread(tifpath, n);
+        eta(n, num_of_frames, starttime, 'cmd', sprintf('Framestack %i/%i\n', index, M), 10);
     end
     
     framestack_raw = single(framestack);
