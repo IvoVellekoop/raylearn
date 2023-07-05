67fdb750707ba6964c2f15114a10fbd656c0c69a
Daniel Cox <danielcox16@hotmail.com>
2023-07-05 18:21:05 +0200

    Separate signal top and bottom

--- process-tpm/plot_report_framestacks.m
+++ process-tpm/plot_report_framestacks.m
@@ -17,36 +17,45 @@ disp('Loading calibration...')
 calibration_data = load(fullfile(dirs.expdata, 'raylearn-data/TPM/calibration/calibration_matrix_parabola/calibration_values.mat'));
 tiffolder = fullfile(dirs.expdata, '/raylearn-data/TPM/TPM-3D-scans/23-Jun-2023_tube-500nL/');
 
-M = 5;  % Number of measurements
+noise_index = {1:1024, 1:140, 100:120};
+signal_index_top = {1:1024, 1:1024, 1:50};
+signal_index_bottom = {1:1024, 1:1024, 70:120};
+
+M = 4;  % Number of measurements
 percentiles = 99.991:0.002:99.999;
 flatpattern_high_signals = zeros(M, numel(percentiles));
-flatpattern_stds = zeros(M, 1);
+flatpattern_stds_top    = zeros(M, 1);
+flatpattern_stds_bottom = zeros(M, 1);
 flatpattern_offsets = zeros(M, 1);
 
+disp('Loading flat pattern measurements...')
 starttime = now;
 for index = 1:M                     % Loop over 'no correction framestack' index
     % Load data
     tiff = load_tiff([tiffolder sprintf('tube-500nL-zoom8-zstep1.400um-no-correction-%i_00001.tif', index)], ...
-        calibration_data, sprintf('Loading frame %i/%i', index, M), true);
+        calibration_data, '', true);
 
     % Compute values from data
     flatpattern_high_signals(index, :) = prctile(tiff.framestack(:), percentiles)';
-    flatpattern_stds(index) = std_framestack_noisecorrect(tiff.framestack, 1:1024, 1:140, 100:120);
+    flatpattern_stds_top(index)    = std_framestack_noisecorrect(tiff.framestack, noise_index, signal_index_top);
+    flatpattern_stds_bottom(index) = std_framestack_noisecorrect(tiff.framestack, noise_index, signal_index_bottom);
     flatpattern_offsets(index) = tiff.offset;
 
-    eta(index, M, starttime, 'cmd', sprintf('Loading framestacks... (%i/%i)', index, M), 0)
+    eta(index, M, starttime, 'cmd', sprintf('Loading framestacks... (%i/%i)', index, M), 0);
 end
 
 %% Report on flat patterns, photobleaching analysis and signal stability
+disp('Flat patterns')
 
 % High signals (percentiles)
 perc_index = 4;
 flatpattern_perc_mean = mean(flatpattern_high_signals(:, perc_index));
 flatpattern_perc_std = std(flatpattern_high_signals(:, perc_index));
-fprintf('\n%.3f%%tile   = %.3g\xB1%.2g\n', percentiles(perc_index), flatpattern_perc_mean, flatpattern_perc_std)
+fprintf('%.3f%%tile   = %.3g\xB1%.2g\n', percentiles(perc_index), flatpattern_perc_mean, flatpattern_perc_std)
 
 % Signal std
-fprintf('Signal \x3C3      = %.3g\xB1%.2g\n', mean(flatpattern_stds), std(flatpattern_stds))
+fprintf('Signal top \x3C3      = %.3g\xB1%.2g\n', mean(flatpattern_stds_top), std(flatpattern_stds_top))
+fprintf('Signal bottom \x3C3   = %.3g\xB1%.2g\n', mean(flatpattern_stds_bottom), std(flatpattern_stds_bottom))
 
 % Signal offset
 fprintf('Signal offset = %.3g\xB1%.2g\n', mean(flatpattern_offsets), std(flatpattern_offsets))
@@ -63,32 +72,33 @@ legend(strcat(num2str(percentiles'), repmat("%tile", [numel(percentiles) 1])))
 set(gca, 'FontSize', 14)
 
 figure;
-plot(flatpattern_stds, '.-')
+plot(flatpattern_stds_top, '.-'); hold on
+plot(flatpattern_stds_bottom, '.-'); hold off
 ylabel('Signal \sigma')
 title('Signal \sigma noise corrected, flat pattern')
+legend('\sigma top', '\sigma bottom')
 set(gca, 'FontSize', 14)
 
 %% Process signals with correction patterns
 
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-RT-1_00001.tif'], calibration_data, flatpattern_stds, 'Top RT 1');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-RT-2_00001.tif'], calibration_data, flatpattern_stds, 'Top RT 2');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-AO-1_00001.tif'], calibration_data, flatpattern_stds, 'Top AO 1');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-AO-2_00001.tif'], calibration_data, flatpattern_stds, 'Top AO 2');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-RT-1_00001.tif'], calibration_data, flatpattern_stds, 'Bottom RT 1');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-RT-2_00001.tif'], calibration_data, flatpattern_stds, 'Bottom RT 2');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-AO-1_00001.tif'], calibration_data, flatpattern_stds, 'Bottom AO 1');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-AO-2_00001.tif'], calibration_data, flatpattern_stds, 'Bottom AO 2');
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-RT-1_00001.tif'], calibration_data, flatpattern_stds_top, noise_index, signal_index_top, 'Top RT 1');
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-RT-2_00001.tif'], calibration_data, flatpattern_stds_top, noise_index, signal_index_top, 'Top RT 2');
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-AO-1_00001.tif'], calibration_data, flatpattern_stds_top, noise_index, signal_index_top, 'Top AO 1');
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-AO-2_00001.tif'], calibration_data, flatpattern_stds_top, noise_index, signal_index_top, 'Top AO 2');
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-RT-1_00001.tif'], calibration_data, flatpattern_stds_bottom, noise_index, signal_index_bottom, 'Bottom RT 1');
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-RT-2_00001.tif'], calibration_data, flatpattern_stds_bottom, noise_index, signal_index_bottom, 'Bottom RT 2');
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-AO-1_00001.tif'], calibration_data, flatpattern_stds_bottom, noise_index, signal_index_bottom, 'Bottom AO 1');
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-AO-2_00001.tif'], calibration_data, flatpattern_stds_bottom, noise_index, signal_index_bottom, 'Bottom AO 2');
 
 
 
 % If we assume that the std error scales with the signal, then the error fraction
 % for both measurements is the same, hence the error fraction of the enhancement is:
 
-function process_tiff(tiffpath, calibration_data, flatpattern_stds, titlestr)
+function process_tiff(tiffpath, calibration_data, flatpattern_stds, noise_index, signal_index, titlestr)
 	tiff = load_tiff(tiffpath, calibration_data);
 	
-    %%%%%%%%%%% Onderscheid maken boven en onder
-	std_framestack = std_framestack_noisecorrect(tiff.framestack, 1:1024, 1:140, 100:120);
+	std_framestack = std_framestack_noisecorrect(tiff.framestack, noise_index, signal_index);
     enhancement = std_framestack ./ mean(flatpattern_stds);
 
     % Note: this is based on the assumption that the error scaling with the signal
