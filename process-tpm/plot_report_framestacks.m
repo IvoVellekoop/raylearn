272f63921ba6f2dae1cb2ec0284a792dde9ec712
Daniel Cox <danielcox16@hotmail.com>
2023-07-14 15:12:35 +0200

    Automatically save plots to pdf

--- process-tpm/plot_report_framestacks.m
+++ process-tpm/plot_report_framestacks.m
@@ -10,7 +10,8 @@ if forcereset || ~exist('dirs', 'var')
     dirconfig_raylearn
 end
 
-do_save = 0;
+do_save = 1;
+savedir = fullfile(dirs.repo, 'plots/3D-scans/');
 
 %%
 disp('Loading calibration...')
@@ -18,8 +19,8 @@ calibration_data = load(fullfile(dirs.expdata, 'raylearn-data/TPM/calibration/ca
 tiffolder = fullfile(dirs.expdata, '/raylearn-data/TPM/TPM-3D-scans/23-Jun-2023_tube-500nL/');
 
 noise_index = {1:1024, 1:140, 100:120};
-signal_index_top = {1:1024, 1:1024, 1:50};
-signal_index_bottom = {1:1024, 1:1024, 70:120};
+signal_index_top = {1:1024, 1:1024, 1:30};
+signal_index_bottom = {1:1024, 1:1024, 90:120};
 
 M = 4;  % Number of measurements
 percentiles = 99.991:0.002:99.999;
@@ -81,21 +82,21 @@ set(gca, 'FontSize', 14)
 
 %% Process signals with correction patterns
 
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-RT-1_00001.tif'], calibration_data, flatpattern_stds_top, noise_index, signal_index_top, 'Top RT 1');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-RT-2_00001.tif'], calibration_data, flatpattern_stds_top, noise_index, signal_index_top, 'Top RT 2');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-AO-1_00001.tif'], calibration_data, flatpattern_stds_top, noise_index, signal_index_top, 'Top AO 1');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-AO-2_00001.tif'], calibration_data, flatpattern_stds_top, noise_index, signal_index_top, 'Top AO 2');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-RT-1_00001.tif'], calibration_data, flatpattern_stds_bottom, noise_index, signal_index_bottom, 'Bottom RT 1');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-RT-2_00001.tif'], calibration_data, flatpattern_stds_bottom, noise_index, signal_index_bottom, 'Bottom RT 2');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-AO-1_00001.tif'], calibration_data, flatpattern_stds_bottom, noise_index, signal_index_bottom, 'Bottom AO 1');
-process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-AO-2_00001.tif'], calibration_data, flatpattern_stds_bottom, noise_index, signal_index_bottom, 'Bottom AO 2');
-
+figure;
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-RT-1_00001.tif'], calibration_data, flatpattern_stds_top, noise_index, signal_index_top, 'Top RT 1', savedir, do_save);
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-RT-2_00001.tif'], calibration_data, flatpattern_stds_top, noise_index, signal_index_top, 'Top RT 2', savedir, do_save);
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-AO-1_00001.tif'], calibration_data, flatpattern_stds_top, noise_index, signal_index_top, 'Top AO 1', savedir, do_save);
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-top-AO-2_00001.tif'], calibration_data, flatpattern_stds_top, noise_index, signal_index_top, 'Top AO 2', savedir, do_save);
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-RT-1_00001.tif'], calibration_data, flatpattern_stds_bottom, noise_index, signal_index_bottom, 'Bottom RT 1', savedir, do_save);
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-RT-2_00001.tif'], calibration_data, flatpattern_stds_bottom, noise_index, signal_index_bottom, 'Bottom RT 2', savedir, do_save);
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-AO-1_00001.tif'], calibration_data, flatpattern_stds_bottom, noise_index, signal_index_bottom, 'Bottom AO 1', savedir, do_save);
+process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-AO-2_00001.tif'], calibration_data, flatpattern_stds_bottom, noise_index, signal_index_bottom, 'Bottom AO 2', savedir, do_save);
 
 
 % If we assume that the std error scales with the signal, then the error fraction
 % for both measurements is the same, hence the error fraction of the enhancement is:
 
-function process_tiff(tiffpath, calibration_data, flatpattern_stds, noise_index, signal_index, titlestr)
+function process_tiff(tiffpath, calibration_data, flatpattern_stds, noise_index, signal_index, titlestr, savedir, do_save)
 	tiff = load_tiff(tiffpath, calibration_data);
 	
 	std_framestack = std_framestack_noisecorrect(tiff.framestack, noise_index, signal_index);
@@ -107,6 +108,34 @@ function process_tiff(tiffpath, calibration_data, flatpattern_stds, noise_index,
     fprintf('\n%s\n', titlestr)
 	fprintf('Signal \x3C3 = %.3g\n', std_framestack)
     fprintf('Signal enhancement = %.3g\xB1%.2g\n', enhancement, enhancement_error)
+
+
+    % Plot
+    um = [' (' 181 'm)'];
+    axesoptions = struct('fontsize', 14);
+    
+    % Plot with im3
+    im3(log(flip(tiff.framestack, 3)), ...
+        'slicedim', 1,...
+        'maxprojection', true,...
+        'title', ['0.5' 181 'm beads in 0.5' 181 'L tube, ' titlestr], ...
+        'dimdata', tiff.dimdata, ...
+        'dimlabels', {['y' um], ['x' um], ['z' um]}, ...
+        'clim', [5 9], ...
+        'axesoptions', axesoptions)
+    colormap inferno
+    
+    fig_resize(550, 1.05);
+    movegui('center')
+    
+    cb = colorbar;
+    ylabel(cb, 'log(signal)')
+    drawnow
+    pause(0.1)
+
+    if do_save
+        [~, filename, ~] = fileparts(tiff.filepath);
+        savepath = fullfile(savedir, [filename '.pdf']);
+        save_figtopdf(savepath);
+    end
 end
-
-
