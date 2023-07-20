f7bbb2a2d17d44a27228847472c70d223160e69f
Daniel Cox <danielcox16@hotmail.com>
2023-07-20 18:20:23 +0200

    Plot horizontal slice as well

--- process-tpm/plot_report_framestacks.m
+++ process-tpm/plot_report_framestacks.m
@@ -21,7 +21,7 @@ tiffolder = fullfile(dirs.expdata, '/raylearn-data/TPM/TPM-3D-scans/23-Jun-2023_
 
 noise_index = {1:1024, 1:140, 100:120};
 signal_index_top = {1:1024, 1:1024, 1:30};
-signal_index_bottom = {1:1024, 1:1024, 90:120};
+signal_index_bottom = {1:1024, 1:1024, 91:120};
 
 M = 4;  % Number of measurements
 percentiles = 99.991:0.002:99.999;
@@ -95,8 +95,6 @@ process_tiff([tiffolder 'tube-500nL-zoom8-zstep1.400um-bottom-AO-2_00001.tif'], 
 
 %% Process/plot/save signals without correction patterns
 for index = 1:M                     % Loop over 'no correction framestack' index
-
-    %%%%%%% For top and bottom?
     process_tiff([tiffolder sprintf('tube-500nL-zoom8-zstep1.400um-no-correction-%i_00001.tif', index)], calibration_data, flatpattern_stds_top, noise_index, signal_index_top, sprintf('No correction %i', index), savedir, do_save);
 end
 
@@ -110,7 +108,7 @@ function process_tiff(tiffpath, calibration_data, flatpattern_stds, noise_index,
 	std_framestack = std_framestack_noisecorrect(tiff.framestack, noise_index, signal_index);
     enhancement = std_framestack ./ mean(flatpattern_stds);
 
-    % Note: this is based on the assumption that the error scaling with the signal
+    % Note: this is based on the assumption that the error scales with the signal
     enhancement_error = sqrt(2) * std(flatpattern_stds) ./ mean(flatpattern_stds);
 
     fprintf('\n%s\n', titlestr)
@@ -118,19 +116,19 @@ function process_tiff(tiffpath, calibration_data, flatpattern_stds, noise_index,
     fprintf('Signal enhancement = %.3g\xB1%.2g\n', enhancement, enhancement_error)
 
 
-    % Plot
+    % === Plot ===
     um = [' (' 181 'm)'];
     axesoptions = struct('fontsize', 14);
     
-    % Plot with im3
-    im3(log(flip(tiff.framestack, 3)), ...
+    % Plot max intensity projection
+    fig = im3(log(flip(tiff.framestack, 3)), ...
         'slicedim', 1,...
         'maxprojection', true,...
         'title', ['0.5' 181 'm beads in 0.5' 181 'L tube, ' titlestr], ...
         'dimdata', tiff.dimdata, ...
         'dimlabels', {['y' um], ['x' um], ['z' um]}, ...
         'clim', [5 9], ...
-        'axesoptions', axesoptions)
+        'axesoptions', axesoptions);
     colormap inferno
     
     fig_resize(550, 1.05);
@@ -138,12 +136,48 @@ function process_tiff(tiffpath, calibration_data, flatpattern_stds, noise_index,
     
     cb = colorbar;
     ylabel(cb, 'log(signal)')
-    drawnow
-    pause(0.1)
 
+    fig.UserData = struct();        % Remove UserData before saving (as this contains the whole framestack!)
+
+    drawnow
+    pause(0.05)
+
+    % Save max intensity projection
     if do_save
         [~, filename, ~] = fileparts(tiff.filepath);
-        savepath = fullfile(savedir, [filename '.pdf']);
-        save_figtopdf(savepath);
+        savepath = fullfile(savedir, [filename '_max-proj.fig']);
+        savefig(savepath);
+        fprintf('Saved as: %s\n', savepath)
+    end
+
+    % Plot horizontal slice
+    fig = im3(log(flip(tiff.framestack, 3)), ...
+        'slicedim', 3,...
+        'slicenum', 17,...
+        'maxprojection', false,...
+        'title', ['0.5' 181 'm beads in 0.5' 181 'L tube, ' titlestr], ...
+        'dimdata', tiff.dimdata, ...
+        'dimlabels', {['y' um], ['x' um], ['z' um]}, ...
+        'clim', [5 9], ...
+        'axesoptions', axesoptions);
+    colormap inferno
+    
+    fig_resize(550, 1.05);
+    movegui('center')
+    
+    cb = colorbar;
+    ylabel(cb, 'log(signal)')
+
+    fig.UserData = struct();        % Remove UserData before saving (as this contains the whole framestack!)
+
+    drawnow
+    pause(0.05)
+
+    % Save horizontal slice
+    if do_save
+        [~, filename, ~] = fileparts(tiff.filepath);
+        savepath = fullfile(savedir, [filename '_hori-slice.fig']);
+        savefig(savepath);
+        fprintf('Saved as: %s\n', savepath)
     end
 end
