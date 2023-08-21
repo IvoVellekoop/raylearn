7f34c878248e61c0871fbf9115971e1b43326186
Daniel Cox <danielcox16@hotmail.com>
2023-08-21 15:00:20 +0200

    Add random pattern

--- experiments-tpm/load_slm_pattern.m
+++ experiments-tpm/load_slm_pattern.m
@@ -4,11 +4,14 @@
 calibrationdata = load("\\ad.utwente.nl\TNW\BMPI\Data\Daniel Cox\ExperimentalData\raylearn-data\TPM\calibration\calibration_matrix_parabola\calibration_values.mat");
 offset_center_slm = calibrationdata.sopt.offset_center_slm;
 
-% Load pattern
+%% Load pattern
 set_RT_pattern(slm, offset_center_slm, "\\ad.utwente.nl\TNW\BMPI\Data\Daniel Cox\ExperimentalData\raylearn-data\pattern-0.5uL-tube-bottom-λ808.0nm.mat")
 
+%% Random pattern to annihilate the focus
+slm.setRect(1, [0 0 1 1]); slm.setData(1, 255*rand(300)); slm.update
 
-% Functions
+
+%% Functions
 function set_RT_pattern(slm, offset_center_slm, matpath)
     patterndata = load(matpath);
     SLM_pattern = flip((angle(patterndata.field_SLM)) * 255 / (2*pi));
