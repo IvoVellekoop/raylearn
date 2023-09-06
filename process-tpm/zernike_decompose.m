5e2eb4257d968babd71abf1a53c867e4ac4eec3c
Daniel Cox <danielcox16@hotmail.com>
2023-09-06 13:50:46 +0200

    Add center and side pattern

--- process-tpm/zernike_decompose.m
+++ process-tpm/zernike_decompose.m
@@ -10,11 +10,17 @@ if forcereset || ~exist('dirs', 'var')
     dirconfig_raylearn
 end
 
-pattern_data = load('/home/dani/LocalData/raylearn-data/TPM/pattern-0.5uL-tube-bottom-λ808.0nm.mat'); titlestr = 'Bottom RT';
-phase_pattern = flip(pattern_data.pathlength_SLM);                  % Unwrapped phase pattern
+% pattern_data = load('/home/dani/LocalData/raylearn-data/TPM/pattern-0.5uL-tube-bottom-λ808.0nm.mat'); titlestr = 'Bottom RT';
+% phase_pattern = flip(pattern_data.phase_SLM);                  % Unwrapped phase pattern
 
 % pattern_data = load('/home/dani/LocalData/raylearn-data/TPM/pattern-0.5uL-tube-top-λ808.0nm.mat'); titlestr = 'Top RT';
-% phase_pattern = flip(pattern_data.pathlength_SLM);                  % Unwrapped phase pattern
+% phase_pattern = flip(pattern_data.phase_SLM);                  % Unwrapped phase pattern
+
+% pattern_data = load('/home/dani/LocalData/raylearn-data/TPM/pattern-0.5uL-tube-center-λ808.0nm.mat'); titlestr = 'Center RT';
+% phase_pattern = flip(pattern_data.phase_SLM);                  % Unwrapped phase pattern
+
+pattern_data = load('/home/dani/LocalData/raylearn-data/TPM/pattern-0.5uL-tube-side-λ808.0nm.mat'); titlestr = 'Side RT';
+phase_pattern = flip(pattern_data.phase_SLM);                  % Unwrapped phase pattern
 
 % pattern_data = load('/mnt/bmpi/Data/Daniel Cox/ExperimentalData/raylearn-data/TPM/adaptive-optics/230620-tube500nL-top/tube_ao_739059.828687_tube500nL-top/tube_ao_739059.828687_tube500nL-top_optimal_pattern.mat'); titlestr = 'Top AO';
 % phase_pattern = pattern_data.slm_pattern_2pi_optimal;                  % Unwrapped phase pattern
@@ -34,7 +40,7 @@ coord_y = linspace(-1, 1, size(phase_pattern, 2))';
 
 circmask = (coord_x.^2 + coord_y.^2) < 1;
 
-slm_rotation_deg = 5.9;
+slm_rotation_deg = 0;
 
 for j_mode = 1:N_modes
     Zcart = circmask .* imrotate(zernfun_cart(coord_x, coord_y, modes(j_mode).n, modes(j_mode).m, false), slm_rotation_deg, "bilinear", "crop");
@@ -51,7 +57,7 @@ ylabel('Amplitude (rad)')
 hold on
 text(4, amplitude(4)-1, ["  4: Vertical Astigmatism (aligned)"])
 text(5, 1, '5: Defocus')
-text(11, 7, ["11: Vertical quadrifoil (aligned)"])
+text(11, 7, ["11: Vertical quadrafoil (aligned)"])
 text(12, 5.5, ["12: 2nd Astigmatism (aligned)"])
 text(13, 4, "13: Primary spherical")
 hold off
