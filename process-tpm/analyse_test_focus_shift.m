ee64c45e6785b9a6101b9b1cb11c6d70ce4aba42
Daniel Cox <danielcox16@hotmail.com>
2023-09-29 09:51:01 +0200

    Change circshift indexing, update file paths

--- process-tpm/analyse_test_focus_shift.m
+++ process-tpm/analyse_test_focus_shift.m
@@ -16,11 +16,11 @@ do_save = 1;
 %% Define directories
 disp('Loading calibration...')
 calibration_data = load(fullfile(dirs.expdata, 'raylearn-data/TPM/calibration/calibration_matrix_parabola/calibration_values.mat'));
-tiffolder = fullfile(dirs.expdata, '/raylearn-data/TPM/TPM-3D-scans/230908_beads_testshift/');
+tiffolder = fullfile(dirs.expdata, '/raylearn-data/TPM/TPM-3D-scans/28-Sep-2023_beads_testshift_xscale0.985200_yscale0.973700/');
 
-tiff_flat = load_tiff([tiffolder 'beads-zoom12-zstep0.250um-no-correction_00001.tif'], calibration_data, '', true); disp('Loaded flat')
-tiff_xshift = load_tiff([tiffolder 'beads-zoom12-zstep0.250um-xshift_00001.tif'], calibration_data, '', true); disp('Loaded x-shift')
-tiff_yshift = load_tiff([tiffolder 'beads-zoom12-zstep0.250um-yshift_00001.tif'], calibration_data, '', true); disp('Loaded y-shift')
+tiff_flat = load_tiff([tiffolder 'beads-zoom10-zstep0.333um-no-correction_00001.tif'], calibration_data, '', true); disp('Loaded flat')
+tiff_xshift = load_tiff([tiffolder 'beads-zoom10-zstep0.333um-xshift_00001.tif'], calibration_data, '', true); disp('Loaded x-shift')
+tiff_yshift = load_tiff([tiffolder 'beads-zoom10-zstep0.333um-yshift_00001.tif'], calibration_data, '', true); disp('Loaded y-shift')
 
 %% Analyze x&y
 
@@ -43,8 +43,8 @@ for i_zslice = slice_range
     count = count+1;
 end
 
-xshift_in_um = mean(xshifts_in_um, 1);
-yshift_in_um = mean(yshifts_in_um, 1);
+xshift_in_um = median(xshifts_in_um, 1);
+yshift_in_um = median(yshifts_in_um, 1);
 xshift_in_um_std = std(vecnorm(xshifts_in_um'));
 yshift_in_um_std = std(vecnorm(yshifts_in_um'));
 
@@ -67,13 +67,19 @@ drawnow
 %% Analyze z
 clear tiff_xshift
 clear tiff_yshift
-tiff_zshift_with_obj = load_tiff([tiffolder 'beads-zoom12-zstep0.250um-zshift_with_obj_zshift_00001.tif'], calibration_data, '', true);  disp('Loaded z-shift')
+tiff_zshift_with_obj = load_tiff([tiffolder 'beads-zoom10-zstep0.333um-zshift_with_obj_zshift_00001.tif'], calibration_data, '', true);  disp('Loaded z-shift')
 thresh = 2000;
-[zshift_in_um, zshift_index, shift_products, i_shifts] = compute_zshift_in_um(bg_median, tiff_flat, tiff_zshift_with_obj, thresh);
+[zshift_in_um, zshift_index, shift_products, shift_steps] = compute_zshift_in_um(bg_median, tiff_flat, tiff_zshift_with_obj, thresh);
 figure;
 
 %% Plot z
-plot(tiff_flat.zstep_um .* (i_shifts - floor((size(shift_products, 2)) / 2)), fftshift(shift_products), '.-')
+[max_innerproduct, i_max_innerproduct] = max(shift_products);
+z_shift_um = tiff_flat.zstep_um .* shift_steps(i_max_innerproduct);
+zshift_step = shift_steps(i_max_innerproduct);
+
+plot(tiff_flat.zstep_um .* shift_steps, shift_products, '.-'); hold on
+plot(z_shift_um, max_innerproduct, 'or'); hold off
+text(z_shift_um, max_innerproduct, sprintf('  z-shift: %.2fum', z_shift_um))
 ylabel('Inner product')
 xlabel('(circular) z-shift (um)')
 title('30um SLM defocus', 'and 30um objective piezo zshift to compensate')
@@ -84,12 +90,12 @@ stackflat = tiff_flat.framestack_raw - bg_median;
 stackshift = tiff_zshift_with_obj.framestack_raw - bg_median;
 stackflat(stackflat < thresh) = 0;
 stackshift(stackshift < thresh) = 0;
-stackflat_shifted = circshift(stackflat, [0 0 -5]);         % Fill in slice shift for z-shift match
+stackflat_shifted = circshift(stackflat, [0 0 zshift_step]);         % Fill in slice shift for z-shift match
 
 defocus_xyshifts_in_um = [];
 
-slice_start = 5+1;
-slice_range = slice_start:size(tiff_flat.framestack_raw, 3)-5;
+slice_start = 1;
+slice_range = slice_start:size(tiff_flat.framestack_raw, 3);
 starttime = now;
 count = 1;
 for i_zslice = slice_range
@@ -99,7 +105,8 @@ for i_zslice = slice_range
     count = count+1;
 end
 
-defocus_xyshift_in_um = mean(defocus_xyshifts_in_um, 1);
+%%
+defocus_xyshift_in_um = median(defocus_xyshifts_in_um, 1);
 defocus_xyshift_in_um_std = std(vecnorm(defocus_xyshifts_in_um'));
 
 %% Plot
@@ -121,6 +128,12 @@ function [shift_in_um, shift_tpmpix] = compute_shift_in_um(bg_median, i_zslice, 
     wienerkinchin_array = wiener_khinchin(tiff_flat.framestack(:,:,i_zslice) - bg_median, tiff_shift.framestack(:,:,i_zslice) - bg_median);
     shift_tpmpix = calculate_offset_in_peak(wienerkinchin_array);
     shift_in_um = Minv * shift_tpmpix' / tiff_flat.zoom;
+
+%     imagesc(fftshift(wienerkinchin_array)'); hold on
+%     plot(shift_tpmpix(1) + size(wienerkinchin_array, 1)/2, shift_tpmpix(2) + size(wienerkinchin_array, 2)/2, 'sw', 'MarkerSize', 15); hold off
+%     title(strcat('Shift: ', num2str(shift_in_um), ' um'))
+%     colorbar
+%     pause(0.3)
 end
 
 function [shift_in_um, shift_tpmpix] = compute_shift_defocus_in_um(i_zslice, array1, array2, Minv, zoom)
@@ -129,7 +142,7 @@ function [shift_in_um, shift_tpmpix] = compute_shift_defocus_in_um(i_zslice, arr
     shift_in_um = Minv * shift_tpmpix' / zoom;
 end
 
-function [zshift_in_um, zshift_index, shift_products, i_shifts] = compute_zshift_in_um(bg_median, tiff_flat, tiff_shift, thresh)
+function [zshift_in_um, zshift_index, shift_products, shift_steps] = compute_zshift_in_um(bg_median, tiff_flat, tiff_shift, thresh)
     stackflat = tiff_flat.framestack_raw - bg_median;
     stackshift = tiff_shift.framestack_raw - bg_median;
     stackflat(stackflat < thresh) = 0;
@@ -137,12 +150,14 @@ function [zshift_in_um, zshift_index, shift_products, i_shifts] = compute_zshift
 
     Nz = size(stackflat, 3);
     shift_products = zeros(1, Nz);
-    i_shifts = 0:(Nz-1);
+    shift_steps = floor(-Nz/2)+1:floor(Nz/2);
     starttime = now;
-    for i_shift = i_shifts
-        stackflat_shifted = circshift(stackflat, [0 0 i_shift]);
-        shift_products(i_shift+1) = mean(stackflat_shifted.*stackshift, [1 2 3]);
-        eta(i_shift+1, Nz, starttime, 'cmd', 'Computing shifted inner products', 0);
+
+    for i_shift = 1:length(shift_steps)
+        shift_step = shift_steps(i_shift);
+        stackflat_shifted = circshift(stackflat, [0 0 shift_step]);
+        shift_products(i_shift) = mean(stackflat_shifted.*stackshift, [1 2 3]);
+        eta(i_shift, Nz, starttime, 'cmd', 'Computing shifted inner products', 0);
     end
 
     [~, zshift_index] = max(shift_products);
